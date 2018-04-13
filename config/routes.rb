b171a976105769628563de2dc2fd9f4162ee4c0a
Sammy Joseph <samuelsadiq52@gmail.com>
2018-04-13 10:19:58 +0100

    Added styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :jobs
   get 'home/index'
 
-  root 'home#index'
+  root 'jobs#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
