f2656c6594850cc58b71b3061be3a488055d1e33
samuelsadiq52@gmail.com <Owner@Adedoyins-MacBook-Pro.local>
2018-04-20 20:27:23 +0100

    first initialize on heroku

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,4 +3,13 @@ class Job < ApplicationRecord
   has_attached_file :job_img, styles: { medium: "300x300>", job_show: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :job_img, content_type: /\Aimage\/.*\z/
 
+  def self.search(search)
+		if search
+			where(["title LIKE ?","%#{search}%"])
+		else
+			all
+		end
+		
+	end
+
 end
